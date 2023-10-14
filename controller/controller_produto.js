d1cbdf197e58b246a9da82c8e1b2333cb8f247f5
luyz.gusta <luizgustavo.sp2020@gmail.com>
2023-10-13 23:45:42 -0300

    arrumado

--- controller/controller_produto.js
+++ controller/controller_produto.js
@@ -69,12 +69,9 @@ const ctlInserirProduto = async (dadosProduto) => {
         dadosProduto.url == '' || dadosProduto.url == null || dadosProduto.url == undefined ||
         dadosProduto.preco_original == null || dadosProduto.preco_original == undefined || 
          dadosProduto.preco_desconto == null || dadosProduto.preco_desconto == undefined || 
-        dadosProduto.preco_original <= dadosProduto.preco_desconto ||        
         dadosProduto.id_tipo_produto == '' || dadosProduto.id_tipo_produto == null || dadosProduto.id_tipo_produto == undefined || isNaN(dadosProduto.id_tipo_produto)
     ){
         return message.ERROR_REQUIRE_FIELDS
-    }else if(dadosProduto.preco_original < dadosProduto.preco_desconto){
-        return message.ERROR_INVALID_VALORES
     } else{
         let resultStatus = await produtosDAO.mdlInsertProduto(dadosProduto)
 
